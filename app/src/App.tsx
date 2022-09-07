f650090907f19bb19f81d37827f6d60fea0eda00
David Feinzeig <feinzeig@gmail.com>
2022-09-07 09:51:42 -0400

    simple app container and app bar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Container from '@mui/material/Container';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+
+import WorkflowStepper from './components/WorkflowStepper/WorkflowStepper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <AppBar>
+        <Typography variant="h6" component="div" align="center">
+          Connect
+        </Typography>
+      </AppBar>
+      <WorkflowStepper></WorkflowStepper>
+    </Container>
   );
 }
 
