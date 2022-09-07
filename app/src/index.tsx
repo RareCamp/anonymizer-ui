b25de670da7a2c936f3c085e86c1f8c88efc174a
David Feinzeig <feinzeig@gmail.com>
2022-09-07 09:52:02 -0400

    good to reset styles

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
